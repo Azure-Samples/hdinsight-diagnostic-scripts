602d8e04edf6f4fa4688fa792276d4c3c4978b70
Mahmood Abushaireh <maabusha@microsoft.com>
2023-05-31 16:06:56 +0300

    get explain

--- HDInsightHiveQueryLogsCollector/lib/hiveUtils.py
+++ HDInsightHiveQueryLogsCollector/lib/hiveUtils.py
@@ -8,4 +8,24 @@ def executeHiveHql(self, hqlFile):
     return result
 
 def generateHiveSetV(self):
-    executeCommand("/usr/bin/hive", "--outputformat=csv2 -n '' -p '' -e 'set -v' > ./results/output/setV.out") 
+    executeHiveQuery(self, 'set -v', "setV.out") 
+
+def executeHiveQuery(self, query, outputFileName):
+    executeCommand("/usr/bin/hive", "--outputformat=csv2 -n '' -p '' -e '{query}' > ./results/output/{outputFileName}".format(query=query, outputFileName=outputFileName)) 
+
+def executeQueryExplain(self, query):
+    # find the query without use or set;
+    statements = query.split(';')
+    for s in statements:
+        if s.startswith('use') or s.startswith('set'):
+            continue
+        else:
+            s = "EXPLAIN {s}".format(s=s)
+    
+    explainQuery = statements.join(';') 
+    # replace newline with spaces;
+    explainQuery = explainQuery.replace('\n', ' ')
+    executeHiveQuery(self, explainQuery, "explainQuery.out")
+
+
+    executeHiveQuery(self, '{query}' , "explainQuery.out") 
